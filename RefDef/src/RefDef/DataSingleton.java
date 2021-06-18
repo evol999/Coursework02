469f68b5d2fd82aa934d7e933990746aa099be63
Beto <evol999@hotmail.com>
2021-06-18 17:43:58 -0500

    SINGLETON
    Added methods to the singleton

--- RefDef/src/RefDef/DataSingleton.java
+++ RefDef/src/RefDef/DataSingleton.java
@@ -5,10 +5,43 @@
  */
 package RefDef;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author evol9
  */
 public class DataSingleton {
 
+    private static DataSingleton instance;
+    public static DataSingleton getInstance() {
+        if (null == instance) {
+            instance = new DataSingleton();
+        }
+        return instance;
+    }
+    private ArrayList<Student> students;
+
+    private DataSingleton() {
+        students = new ArrayList<>();
+    }
+
+    /**
+     * @return the students
+     */
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public String getStudentNameByID(int id) {
+        String retVal = null;
+
+        for (Student i : students) {
+            if (i.getStudentID() == id) {
+                retVal = i.getName();
+            }
+        }
+
+        return retVal;
+    }
 }
