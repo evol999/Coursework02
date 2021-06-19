39ed0ea87cfba6c4a591b956dafa141bbf57872f
Beto <evol999@hotmail.com>
2021-06-19 15:25:54 -0500

    NEW CLASS
    Added class for subject management.

---
+++ RefDef/src/RefDef/Subject.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RefDef;
+
+/**
+ *
+ * @author evol9
+ */
+public class Subject {
+
+    /**
+     * @return the subjectID
+     */
+    public int getSubjectID() {
+        return subjectID;
+    }
+
+    /**
+     * @param subjectID the subjectID to set
+     */
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    private int subjectID;
+    private String name;
+    private double price;
+}
