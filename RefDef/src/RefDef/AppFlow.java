536559d3f092885d5f81611f8899d7b234af0c32
Beto <evol999@hotmail.com>
2021-06-21 17:19:11 -0500

    alreadyBooked
    Implementation.

--- RefDef/src/RefDef/AppFlow.java
+++ RefDef/src/RefDef/AppFlow.java
@@ -107,7 +107,7 @@ class AppFlow {
                     console.notEmpty(tempLesson);
                     break;
                 case ALREADY_BOOKED:
-                    console.alreadyBooked();
+                    console.alreadyBooked(tempLesson);
                     break;
                 default:
                     break;
